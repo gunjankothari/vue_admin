ed0a7742107c4b7a88e48101a22fa8b036191a93
Rahul Adroja <aadarojarahul@gmail.com>
2018-09-24 19:18:33 +0530

    User listing

--- src/admin/pages/users/users.vue
+++ src/admin/pages/users/users.vue
@@ -5,16 +5,19 @@
                 <breadcrumb></breadcrumb>
             </b-col>
         </b-row>
+        <users-info-grid></users-info-grid>
     </b-container>
 </template>
 
 <script>
 import * as _ from 'lodash';
-import breadcrumb from '../components/breadcrumb';
+import breadcrumb from '../../components/breadcrumb';
+import UsersInfoGrid from './usersInfoGrid'
 
     export default {
         components: {
-            breadcrumb
+            breadcrumb,
+            UsersInfoGrid
         }
     }
 </script>
