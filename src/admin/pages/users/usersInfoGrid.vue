67df32943a7cb1aca75ad87fdbed71b5388bccf2
RahulAadroja <aadarojarahul@gmail.com>
2018-09-27 14:12:04 +0530

    User Listing Search and Export Functionality

--- src/admin/pages/users/usersInfoGrid.vue
+++ src/admin/pages/users/usersInfoGrid.vue
@@ -1,159 +1,125 @@
 <template>
-<div>
-    <b-container fluid class="container-main">
-        <b-row>
-            <b-col lg="3" md="12" sm="12">
-                <b-input-group size="md" class="mb-3 search-box " >
+    <div>
+        <b-container fluid class="container-main">
+            <b-row>
+                <b-col lg="2" md="9" sm="9" class="col-xs-12 col-sm-6 col-md-3 example-col">
+                    <b-input-group size="md" class="mb-3 search-box " >
+                        <b-form-input id="search-textbox" placeholder="Search users..." v-model="searchUserText" @keyup.native="userGridSearch()"></b-form-input>
+                    </b-input-group>
+                </b-col>
+            </b-row>
+        </b-container>
+        <div id="vueapp" class="vue-app" >
+            <kendo-grid
+                :height="500"
+                :data-source="users"
+                :resizable="true"
+                :filterable="true"
+                :sortable-allow-unsort="true"
+                :sortable-show-indexes="true"
+                :scrollable-virtual="true"
+                :pageable-numeric="false"
+                :pageable-previous-next="false"
+                :pageable-messages-display="'Showing {2} users'"
+                :editable="'popup'"
+                :toolbar="[{name: 'excel', text: 'Excel'}]"
+                :excel-file-name="'Motadata_UserListing.xlsx'"
+                :excel-filterable="true"
+            >
+                <kendo-grid-column :selectable="true" :width="35"></kendo-grid-column>
+                <kendo-grid-column :field="'UserName'"
+                                   :template="userNameTemplate"
+                                   :width="180"></kendo-grid-column>
+                <kendo-grid-column :field="'Role'"
+                                   :width="140"></kendo-grid-column>
+                <kendo-grid-column :field="'AssignedGroups'"
+                                   :width="180" ></kendo-grid-column>
+                <kendo-grid-column :field="'Email'"
+                                   :width="220" ></kendo-grid-column>
+                <kendo-grid-column :field="'Description'"
+                                   :width="220" ></kendo-grid-column>
 
-                    <b-form-input placeholder="Search users..."></b-form-input>
-
-                    <b-input-group-append>
-                        <b-btn class="search-button" variant="primary" @click="userGridSearch()">
-                            <icon class="icon-search" name="search" size="sm"></icon>
-                        </b-btn>
-                    </b-input-group-append>
-                </b-input-group>
-            </b-col>
-        </b-row>
-    </b-container>
-    <div >
-        <kendo-grid :data-source="users"
-                    :height="600"
-                    :resizable="true"
-                    :sortable-allow-unsort="true"
-                    :sortable-show-indexes="true"
-                    :scrollable-virtual="true"
-                    :pageable-numeric="false"
-                    :pageable-previous-next="false"
-                    :pageable-messages-display="'Showing {2} users'"
-                    :editable="'popup'"
-
-        >
-            <kendo-grid-column :selectable="true" :width="35"></kendo-grid-column>
-            <kendo-grid-column :field="'UserName'"
-                                :template="userNameTemplate"
-                               :width="180"></kendo-grid-column>
-            <kendo-grid-column :field="'Role'"
-                               :width="140"></kendo-grid-column>
-            <kendo-grid-column :field="'AssignedGroups'"
-                               :width="180" ></kendo-grid-column>
-            <kendo-grid-column :field="'Email'"
-                               :width="220" ></kendo-grid-column>
-            <kendo-grid-column :field="'Description'"
-                               :width="220" ></kendo-grid-column>
-
-            <kendo-grid-column :text="'Actions'"
-                               :command="['edit', 'destroy']"
-                               :title="'Actions'"
-                               :width="150"></kendo-grid-column>
-        </kendo-grid>
+                <kendo-grid-column :text="'Actions'"
+                                   :command="['edit', 'destroy']"
+                                   :title="'Actions'"
+                                   :width="150"></kendo-grid-column>
+            </kendo-grid>
+        </div>
     </div>
-</div>
+
 </template>
 
 <script>
-    import usersStore from '../users/users-store';
-
+    import JsZip from 'jszip';
+    window.JSZip = JsZip;
     export default {
-        name: "usersInfoGrid",
-        methods:{
-            userGridSearch:function (ev) {
-
-                var grid = $('#grid').data('kendoGrid');
-                var columns = grid.columns;
-            }
-        },
         data () {
             return {
-                userNameTemplate: '<label class="switch" ><input type="checkbox" name="UserName" id="UserName" # if(Status){ # checked # } #  @click="CheckboxClick()"/><span class="slider round"></span><span class="grid-user">\#: UserName#\</span></label>',
+                tempStore : this.$store.state.usersStore.state.users,
+                searchUserText:'',
+                userNameTemplate: '<label class="switch" ><input type="checkbox"   @click="checkboxToggle()" # if(Status){ # checked # } # /><span class="slider round"></span><span class="grid-user">\#: UserName#\</span></label>',
             }
         },
         computed: {
             users () {
-                debugger;
                 return new kendo.data.DataSource({
                     data: this.$store.state.usersStore.state.users
                 })
             }
+        },
+        methods:{
+            userGridSearch:function () {
+
+                var searchText = this.searchUserText;
+
+                if(searchText != "" && searchText !=undefined) {
+                    this.$store.state.usersStore.state.users = this.userGridFilter(searchText)
+                }
+                else{
+                    this.$store.state.usersStore.state.users = this.tempStore
+                }
+            },
+            checkboxToggle : function(dataItem)
+            {
+                //TODO Grid checkbox template event binding not working
+              alert("Rahul")
+            },
+            userGridFilter:function (searchText) {
+
+                var newArray=[];
+                var users = this.tempStore.map( user => {
+
+                    var flag = false;
+
+                    if(user.UserName.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                        flag = true;
+                    }
+                    else if(user.Role.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                        flag = true;
+                    }
+                    else if(user.AssignedGroups.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                        flag = true;
+                    }
+                    else if(user.Email.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                        flag = true;
+                    }
+                    else if(user.Description.toLowerCase().indexOf(searchText.toLowerCase())>-1){
+                        flag = true;
+                    }
+                    else {
+                        flag = false;
+                    }
+                    if(flag){
+                        newArray.push(user);
+                    }
+                });
+                return newArray;
+            }
         }
-
-
-
     }
+
 </script>
 
-<style type="text/css">
-.grid-user{
-    position: relative;
-    left: 63px;
-}
-.search-box{
-    box-shadow: 0 1px 4px 0 rgba(86,108,118,0.12);
-    border: none;
+<style lang="less" scoped>
 
-}
-.search-button{
-    background-color: rgba(0,152,238,1);
-}
-.icon-search {
-    height: 12px;
-}
-.switch {
-    position: relative;
-    display: inline-block;
-    width: 51px;
-    height: 27px;
-}
-
-.switch input {display:none;}
-
-.slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: .4s;
-    transition: .4s;
-}
-
-.slider:before {
-    position: absolute;
-    content: "";
-    height: 19px;
-    width: 19px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: .4s;
-    transition: .4s;
-}
-
-input:checked + .slider {
-    background-color: #F4D03F;
-}
-
-input:focus + .slider {
-    box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-    border-radius: 34px;
-}
-
-.slider.round:before {
-    border-radius: 50%;
-}
-.k-grid-header{
-    font-weight: bold !important;
-}
 </style>
