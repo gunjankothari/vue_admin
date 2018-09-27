4efe975f9073dc23c76474db532f63451ca298c4
gunjankothari <gunjan.kothari2002@gmail.com>
2018-09-27 08:23:18 +0530

    Sidebar

--- src/admin/pages/users/users.vue
+++ src/admin/pages/users/users.vue
@@ -1,11 +1,24 @@
 <template>
     <b-container fluid class="container-main">
         <b-row>
-            <b-col>
-                <breadcrumb></breadcrumb>
+            <b-col cols="0" sm="3" lg="2" class="sidebar d-none d-sm-flex">
+                <div>
+                    Sidebar
+                </div>
             </b-col>
+            <b-col cols="12" sm="9" lg="10"> 
+                <b-row>
+                    <b-col>
+                        <breadcrumb></breadcrumb>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <users-info-grid></users-info-grid>
+                    </b-col>
+                </b-row>
+            </b-col>            
         </b-row>
-        <users-info-grid></users-info-grid>
     </b-container>
 </template>
 
@@ -24,7 +37,11 @@ import UsersInfoGrid from './usersInfoGrid'
 
 <style lang="less" scoped>
     .container-main {
-        padding: 35px 80px;
         background: #fcfcfc;
+    }
+    .sidebar{
+        display: flex;
+        width: 300px;
+        background: #eee;
     }
 </style>
